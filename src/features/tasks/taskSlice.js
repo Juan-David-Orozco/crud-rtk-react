a2e8352a2105e4fc80cf5b49a854051104b184fc
Juan-David-Orozco <ing.ele2018@gmail.com>
2023-02-06 21:08:27 -0500

    initial config - redux toolkit

---
+++ src/features/tasks/taskSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const taskSlice = createSlice({
+  name: 'tasks',
+  initialState: [],
+  reducers: {
+
+  }
+})
+
+export default taskSlice.reducer
