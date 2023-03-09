2c0dc0af122f4edcf1b51c380f64aade658721ae
Juan-David-Orozco <ing.ele2018@gmail.com>
2023-03-09 11:41:36 -0500

    actualizacion deploy app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 import { store } from './app/store'
 import { Provider } from 'react-redux'
 
@@ -13,3 +14,5 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+reportWebVitals();
